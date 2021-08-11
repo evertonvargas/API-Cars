78650602a5844bff1dc1a8b9ec05d95e92f96763
evertonvargas <evertonpassa7@gmail.com>
2021-08-10 22:27:31 -0300

    add tipagem

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { Category } from "../model/Category";
 
 export const categoriesRoutes = Router();
 
-const categories = [];
+const categories:Category [] = [];
 
-categoriesRoutes.post("/categories", (request, response)=>{
+categoriesRoutes.post("/", (request, response)=>{
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
-    name,
+  const category = new Category();
+  Object.assign(category, {
+    name, 
     description,
-    created_at : new Date()
-  }
+    created_at: new Date()
+  })
 
   categories.push(category)
 
-  return response.status(201).send();
+  return response.status(201).send(category);
 })
